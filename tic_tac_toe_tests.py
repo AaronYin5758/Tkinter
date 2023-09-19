e47098164f9ca65dd616c53a374c1ce521615697
AaronYin5758 <yinaaron5758@gmail.com>
2023-09-18 17:45:03 -0700

    removed set_up_screen, added a few helper functions, changed board to 1d list of char for easy testing

---
+++ tic_tac_toe_tests.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+import random
+from tic_tac_toe_AI import *
+
+def main():
+
+    test_random()
+
+def print_board():
+    # for debugging purposes
+    global board
+    for row in range(3):
+        for col in range(3):
+            if col == 2:
+                print(board[row][col]['text'])
+            else:
+                print(board[row][col]['text'], end = ",")
+    
+def buttonify(b):
+    # transforms a 1d list of chars into a 2d list of buttons to be used in ai file
+    # this is for easier set up of test cases
+    board = [[0,0,0], [0,0,0], [0,0,0]]
+    for i, char in enumerate(b):
+        board[i//3][i%3] = tk.Button(text=char)
+    return board
+    
+def test_random():
+    board = [0]*9
+    for _ in range(100):
+        for i in range(9):
+            board[i] = "1" if random.randint(0,1) else " "
+        board[2] = " "
+        for _ in range(100):
+            row, col = random_ai(buttonify(board), "", "")
+            #print(row, col)
+            if board[row*3 + col] != " ":
+                print("error")
+                print("row:", row, ", col:", col)
+                print_board()
+                return False
+    print("random_ai passed")
+
+def test_inter():
+    global board
+
+
+if __name__ == '__main__':
+    main()
