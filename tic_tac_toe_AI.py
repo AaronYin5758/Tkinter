a8a73f177350ddc0d3895724783bf950ad3de299
AaronYin5758 <yinaaron5758@gmail.com>
2023-09-20 19:02:01 -0700

    completed hard ai by using alpha beta pruning, updated check_winner, parse_board, random and inter ai to be ignorant towards ai and player symbols.

--- tic_tac_toe_AI.py
+++ tic_tac_toe_AI.py
@@ -5,37 +5,57 @@
 #                               - output: the index where the AI will make its move
 import tkinter as tk
 import random
+from math import inf
 
+def check_winner(board:list[str]) -> (bool, str):
+    # checks the board and returns True and the winning side or False and None
+    if board[0] == board[1] == board[2]:  #check 3 rows
+        return True, board[0]
+    elif board[3] == board[4] == board[5]:
+        return True, board[3]
+    elif board[6] == board[7] == board[8]:
+        return True, board[6]
+    elif board[0] == board[3] == board[6]:  #check 3 columns
+        return True, board[0]
+    elif board[1] == board[4] == board[7]:
+        return True, board[1]
+    elif board[2] == board[5] == board[8]:
+        return True, board[2]
+    elif board[0] == board[4] == board[8]:  #check 2 diagonals
+        return True, board[0]
+    elif board[2] == board[4] == board[6]:
+        return True, board[2]
+    empty = board.count(" ")
+    if empty > 0:
+        return False, None 
+    else:
+        return False, "DRAW"
 
-def isWinner(board, symbol):
-    return (board[0] == symbol and board[1] == symbol and board[2] == symbol or 
-            board[3] == symbol and board[4] == symbol and board[5] == symbol or 
-            board[6] == symbol and board[7] == symbol and board[8] == symbol or #3 rows checked
-            board[0] == symbol and board[3] == symbol and board[6] == symbol or 
-            board[1] == symbol and board[4] == symbol and board[7] == symbol or 
-            board[2] == symbol and board[5] == symbol and board[8] == symbol or #3 columns checked
-            board[0] == symbol and board[4] == symbol and board[8] == symbol or 
-            board[2] == symbol and board[4] == symbol and board[6] == symbol) #2 crosses checked
-
-def parse_board(board):
+def parse_board(board:list[list[tk.Button]], symbol:str, player:str)->(list[int], list[int]):
     # transform the 2d list of buttons to a 1d list of chars
+    # also changes both players' symbols to 1 and -1 respectively, this makes implementing hard ai easier
     # the indexes are [0, 8] for consistency
     b, empty = [], []
     for row in range(3):
         for col in range(3):
-            b.append(board[row][col]['text'])
-            if b[-1] == " ":
+            if board[row][col]['text'] == symbol:
+                b.append("1")
+            elif board[row][col]['text'] == player:
+                b.append("-1")
+            else:
+                b.append(" ")
                 empty.append(len(b)-1)
+    #1print("Parse:", b, empty)
     return (b, empty)
 
 def index_to_2d(i):
     # convert 1d index from 0-8 to 2d index (row, col), row, col in [0, 3]
     return (i//3, i%3)
 
-def random_ai(board, symbol, player):
+def random_ai(board, symbol, player)->(int, int):
     #chooses an available tile randomly
-    _, empty = parse_board(board)
-    return index_to_2d(random.choice(empty)) if empty else -1
+    _, empty = parse_board(board, symbol, player)
+    return index_to_2d(random.choice(empty)) if empty else (-1,-1)
 
 def inter_ai(board, symbol, player):
     # ai looks at state of board, then decides its move greedily based on the following
@@ -44,7 +64,7 @@ def inter_ai(board, symbol, player):
     #           2.prevent player from winning 
     #           3.choose the center, a corner, or a middles tile (the tile with most "empty" directions)
     ###############################################################
-    b, empty = parse_board(board)
+    b, empty = parse_board(board, symbol, player)
 
     if not empty: # full board, something wrong
         return -1
@@ -61,11 +81,13 @@ def inter_ai(board, symbol, player):
     self_win, player_win = None, None
     for ind in empty:    
         temp_b = b[:]
-        temp_b[ind] = symbol
-        if isWinner(temp_b, symbol):
+        temp_b[ind] = "1"
+        game_won, winner = check_winner(temp_b)
+        if game_won and winner == "1":
             self_win = ind
-        temp_b[ind] = player
-        if isWinner(temp_b, player):
+        temp_b[ind] = "-1"
+        game_won, winner = check_winner(temp_b)
+        if game_won and winner == "-1":
             player_win = ind
     
     # prioritize self win over player win
@@ -80,4 +102,43 @@ def inter_ai(board, symbol, player):
 
 
 def hard_ai(board, symbol, player):
-    pass
+    # use alpha beta pruning to determine which tile to pick
+    b, empty = parse_board(board)
+    res = alpha_beta(b, empty, inf, -inf, "1")
+    return res[0]
+
+def alpha_beta(board, empty, alpha, beta, cur_symbol):
+    # returns position, score
+    # "1" is ai/self, "-1" is player thus self winning scores positive, and tie score neutral
+    # alpha/beta are the upper/lower limits
+    # cur_symbol measures the current states, whether it's alpha or beta
+    game_won, winner = check_winner(board)
+    pos = -1
+    if len(empty) == 0 or winner:
+        score = 0
+        if winner == "1": score = 1
+        elif winner == "-1": score = -1
+        return [pos, score]
+    
+    for tile in empty:
+        board[tile] = cur_symbol
+        temp_e = empty[:]
+        temp_e.remove(tile)
+        res, score = alpha_beta(board, temp_e, alpha, beta, str(-int(cur_symbol)))
+        if cur_symbol == "1":
+            # max node
+            if score > alpha: # update alpha
+                alpha, pos = score, res
+        else:
+            if score < beta:
+                beta, pos = score, res
+
+        board[tile] = " "  # reset(backtrack)
+
+        if alpha >= beta:  # cut off branch
+            break
+
+    if cur_symbol == "1":
+        return pos, alpha
+    else:
+        return pos, beta
